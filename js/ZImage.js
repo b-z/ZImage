b82927b74858ab9ab97e5ea177f39d87bed2edc8
Zhou Bowei <zbwwwww@gmail.com>
2015-11-16 18:48:32 +0800

    add usm & saturation

--- js/ZImage.js
+++ js/ZImage.js
@@ -83,6 +83,13 @@ ZImage.getPixelRGB = function(dataArray, x, y) {
 	}
 }
 
+ZImage.setPixelRGB = function(dataArray, x, y, color) {
+	dataArray[y * this.width * 4 + x * 4] = color.r;
+	dataArray[y * this.width * 4 + x * 4 + 1] = color.g;
+	dataArray[y * this.width * 4 + x * 4 + 2] = color.b;
+	dataArray[y * this.width * 4 + x * 4 + 3] = color.a;
+}
+
 ZImage.getPixelSingle = function(dataArray, x, y) {
 	if (x < 0) return this.getPixelSingle(dataArray, 0, y);
 	if (y < 0) return this.getPixelSingle(dataArray, x, 0);
@@ -90,201 +97,6 @@ ZImage.getPixelSingle = function(dataArray, x, y) {
 	if (y >= this.height) return this.getPixelSingle(dataArray, x, this.height - 1);
 	return dataArray[y * this.width + x];
 }
-
-/****************************************************************
- * Fourier Transform
- */
-//
-// ZImage.fastFourierTransformOneDimension = function(dataArray) {
-// 	this.mul = function(a, b) {
-// 		if (typeof(a) !== 'object') {
-// 			a = {
-// 				real: a,
-// 				imag: 0
-// 			}
-// 		}
-// 		if (typeof(b) !== 'object') {
-// 			b = {
-// 				real: b,
-// 				imag: 0
-// 			}
-// 		}
-// 		return {
-// 			real: a.real * b.real - a.imag * b.imag,
-// 			imag: a.real * b.imag + a.imag * b.real
-// 		};
-// 	};
-// 	this.add = function(a, b) {
-// 		if (typeof(a) !== 'object') {
-// 			a = {
-// 				real: a,
-// 				imag: 0
-// 			}
-// 		}
-// 		if (typeof(b) !== 'object') {
-// 			b = {
-// 				real: b,
-// 				imag: 0
-// 			}
-// 		}
-// 		return {
-// 			real: a.real + b.real,
-// 			imag: a.imag + b.imag
-// 		};
-// 	};
-// 	this.sub = function(a, b) {
-// 		if (typeof(a) !== 'object') {
-// 			a = {
-// 				real: a,
-// 				imag: 0
-// 			}
-// 		}
-// 		if (typeof(b) !== 'object') {
-// 			b = {
-// 				real: b,
-// 				imag: 0
-// 			}
-// 		}
-// 		return {
-// 			real: a.real - b.real,
-// 			imag: a.imag - b.imag
-// 		};
-// 	};
-// 	this.sort = function(data, r) {
-// 		if (data.length <= 2) {
-// 			return data;
-// 		}
-// 		var index = [0, 1];
-// 		for (var i = 0; i < r - 1; i++) {
-// 			var tempIndex = [];
-// 			for (var j = 0; j < index.length; j++) {
-// 				tempIndex[j] = index[j] * 2;
-// 				tempIndex[j + index.length] = index[j] * 2 + 1;
-// 			}
-// 			index = tempIndex;
-// 		}
-// 		var datatemp = [];
-// 		for (var i = 0; i < index.length; i++) {
-// 			datatemp.push(data[index[i]]);
-// 		}
-// 		return datatemp;
-// 	};
-// 	var dataLen = dataArray.length;
-// 	var r = 1;
-// 	var i = 1;
-// 	while (i * 2 < dataLen) {
-// 		i *= 2;
-// 		r++;
-// 	}
-// 	var count = 1 << r;
-// 	for (var i = dataLen; i < count; i++) {
-// 		dataArray[i] = 0;
-// 	}
-// 	dataArray = this.sort(dataArray, r);
-// 	var w = [];
-// 	for (var i = 0; i < count / 2; i++) {
-// 		var angle = -i * Math.PI * 2 / count;
-// 		w.push({
-// 			real: Math.cos(angle),
-// 			imag: Math.sin(angle)
-// 		});
-// 	}
-// 	for (var i = 0; i < r; i++) {
-// 		var group = 1 << (r - 1 - i);
-// 		var distance = 1 << i;
-// 		var unit = 1 << i;
-// 		for (var j = 0; j < group; j++) {
-// 			var step = 2 * distance * j;
-// 			for (var k = 0; k < unit; k++) {
-// 				var temp = this.mul(dataArray[step + k + distance], w[count * k / 2 / distance]);
-// 				dataArray[step + k + distance] = this.sub(dataArray[step + k], temp);
-// 				dataArray[step + k] = this.add(dataArray[step + k], temp);
-// 			}
-// 		}
-// 	}
-// 	return dataArray;
-// }
-//
-// ZImage.fastFourierTransform = function(dataArray) {
-// 	var width = this.width;
-// 	var height = this.height;
-// 	var r = 1;
-// 	var i = 1;
-// 	while (i * 2 < width) {
-// 		i *= 2;
-// 		r++;
-// 	}
-// 	var width2 = 1 << r;
-// 	var r = 1;
-// 	var i = 1;
-// 	while (i * 2 < height) {
-// 		i *= 2;
-// 		r++;
-// 	}
-// 	var height2 = 1 << r;
-// 	var dataArrayTemp = [];
-// 	for (var i = 0; i < height2; i++) {
-// 		for (var j = 0; j < width2; j++) {
-// 			if (i >= height || j >= width) {
-// 				dataArrayTemp.push(0);
-// 			} else {
-// 				dataArrayTemp.push(dataArray[i * width + j]);
-// 			}
-// 		}
-// 	}
-// 	dataArray = dataArrayTemp;
-// 	width = width2;
-// 	height = height2;
-// 	var dataTemp = [];
-// 	var dataArray2 = [];
-// 	for (var i = 0; i < height; i++) {
-// 		dataTemp = [];
-// 		for (var j = 0; j < width; j++) {
-// 			dataTemp.push(dataArray[i * width + j]);
-// 		}
-// 		dataTemp = this.fastFourierTransformOneDimension(dataTemp);
-// 		for (var j = 0; j < width; j++) {
-// 			dataArray2.push(dataTemp[j]);
-// 		}
-// 	}
-// 	dataArray = dataArray2;
-// 	dataArray2 = [];
-// 	for (var i = 0; i < width; i++) {
-// 		var dataTemp = [];
-// 		for (var j = 0; j < height; j++) {
-// 			dataTemp.push(dataArray[j * width + i]);
-// 		}
-// 		dataTemp = this.fastFourierTransformOneDimension(dataTemp);
-// 		for (var j = 0; j < height; j++) {
-// 			dataArray2.push(dataTemp[j]);
-// 		}
-// 	}
-// 	dataArray = [];
-// 	for (var i = 0; i < height; i++) {
-// 		for (var j = 0; j < width; j++) {
-// 			var t = dataArray2[i * width + j];
-// 			dataArray[j * height + i] = t;
-// 		}
-// 	}
-// 	return dataArray;
-// }
-//
-// ZImage.fftShow = function(dataArray) {
-// 	var min = 1e10;
-// 	var max = -1e10;
-// 	var dst=[];
-// 	for (var i = 0; i < dataArray.length; i++) {
-// 		var t = dataArray[i];
-// 		var s = Math.log(1 + Math.sqrt(Math.pow(t.real, 2), Math.pow(t.imag, 2)));
-// 		if (s > max) max = s;
-// 		if (s < min) min = s;
-// 		dst.push(s);
-// 	}
-// 	for (var i = 0; i < dataArray.length; i++) {
-// 		dst[i] = (dst[i] - min) / (max - min);
-// 	}
-// 	return dst;
-// }
 
 /****************************************************************
  * Blur
@@ -368,5 +180,113 @@ ZImage.generateGussianBlurKernel = function(size, sigma) {
 	};
 }
 
-// mask=[[1/9,1/9,1/9],[1/9,1/9,1/9],[1/9,1/9,1/9]];
-// ZImage.imgr=ZImage.convolute(mask,ZImage.imgr,{x:1,y:1});
+/****************************************************************
+ * USM
+ */
+
+ZImage.usm = function(dataArray, amount, radius, threshold) {
+	var len = dataArray.length;
+	var res = [];
+	for (var i = 0; i < len; i++) {
+		res.push(0);
+	}
+	var kernel = this.generateGussianBlurKernel(radius, (radius - 1) / 3);
+	var smoothArray = this.convolute(kernel.mask, dataArray, kernel.center);
+	// console.log(smoothArray);
+	for (var i = 0; i < this.width; i++) {
+		for (var j = 0; j < this.height; j++) {
+			var n = j * this.width + i;
+			var value = dataArray[n] - smoothArray[n];
+			if (Math.abs(value) > threshold) {
+				value = dataArray[n] + amount * value / 100;
+				value = value > 255 ? 255 : value;
+				value = value < 0 ? 0 : value;
+				value = Math.round(value);
+				res[n] = value; //(value | ((255 - value) >> 15)) & ~value >> 15;
+			} else {
+				res[n] = dataArray[n];
+			}
+		}
+	}
+	return res;
+}
+
+/****************************************************************
+ * Saturation
+ */
+ZImage.rgb2hsl = function(r, g, b) {
+	r /= 255;
+	g /= 255;
+	b /= 255;
+	var max = Math.max(r, g, b),
+		min = Math.min(r, g, b),
+		h, s, l = (max + min) / 2;
+	if (max === min) {
+		h = s = 0; // achromatic
+	} else {
+		var d = max - min;
+		s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
+		switch (max) {
+			case r:
+				h = (g - b) / d + (g < b ? 6 : 0);
+				break;
+			case g:
+				h = (b - r) / d + 2;
+				break;
+			case b:
+				h = (r - g) / d + 4;
+				break;
+		}
+		h /= 6;
+	}
+	return {h:h, s:s, l:l};
+};
+
+ZImage.hsl2rgb = function(h, s, l) {
+	var r, g, b,
+		hue2rgb = function(p, q, t) {
+			if (t < 0) {
+				t += 1;
+			}
+			if (t > 1) {
+				t -= 1;
+			}
+			if (t < 1 / 6) {
+				return p + (q - p) * 6 * t;
+			}
+			if (t < 1 / 2) {
+				return q;
+			}
+			if (t < 2 / 3) {
+				return p + (q - p) * (2 / 3 - t) * 6;
+			}
+			return p;
+		};
+	if (s === 0) {
+		r = g = b = l; // achromatic
+	} else {
+		var
+			q = l < 0.5 ? l * (1 + s) : l + s - l * s,
+			p = 2 * l - q;
+		r = hue2rgb(p, q, h + 1 / 3);
+		g = hue2rgb(p, q, h);
+		b = hue2rgb(p, q, h - 1 / 3);
+	}
+	return {r:r * 0xFF, g:g * 0xFF, b:b * 0xFF};
+};
+
+ZImage.setSaturation = function(amount) {
+	for (var i = 0; i < this.width; i++) {
+		for (var j = 0; j < this.height; j++) {
+			var p = this.getPixelRGB(this.img.data, i, j);
+			var hsl = this.rgb2hsl(p.r, p.g, p.b);
+			hsl.s *= amount / 100;
+			hsl.s = hsl.s > 1 ? 1 : hsl.s;
+			hsl.s = hsl.s < 0 ? 0 : hsl.s;
+			var rgb = this.hsl2rgb(hsl.h, hsl.s, hsl.l);
+			rgb.a = p.a;
+			this.setPixelRGB(this.img.data, i, j, rgb);
+		}
+	}
+	this.ctx.putImageData(this.img, 0, 0);
+}
